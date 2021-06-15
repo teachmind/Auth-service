005859ac281221a6f9d0579899587268cc98da46
Md Riadul Islam <rdhawladar@gmail.com>
2021-06-16 00:07:51 +0600

    AP-1 | [BE] Add health check api (#1)
    * AP-1 | [BE] Add health check api
    
    * AP-1 | [BE] Removed test API and added vendor to .gitignore
    
    Co-authored-by: Md Riadul Islam <riadul.islam@bongobd.com>

---
+++ cmd/server.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+	"user-service/internal/app/server"
+	"user-service/internal/app/token"
+	"user-service/internal/pkg/postgres"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+)
+
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Start server",
+	Long:  `Start server`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		db, err := postgres.New(&postgres.Config{
+			Host:     os.Getenv("DB_HOST"),
+			Port:     os.Getenv("DB_PORT"),
+			User:     os.Getenv("DB_USER"),
+			Password: os.Getenv("DB_PASSWORD"),
+			Name:     os.Getenv("DB_NAME"),
+		})
+		println(db)
+
+		if err != nil {
+			panic(err)
+		}
+		s := server.NewServer(os.Getenv("APP_PORT"),
+			token.NewService(),
+		)
+
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+		go func() {
+			<-sig
+			if err := s.Shutdown(); err != nil {
+				log.Error().Err(err).Msg("error during server shutdown")
+			}
+		}()
+
+		return s.Run()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+}
