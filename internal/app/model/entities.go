f8e2c5c260b92fb8fcdc2b288e149c62efb206e1
Md. Asaduzzaman Noor <md.noor.asad@gmail.com>
2021-06-23 15:00:33 +0600

    Feature/ap 2 (#2)
    * AP-2 | [BE] Add database migration code for auth service.
    
    * AP-2 | [BE] Environment variable setup code commented.
    
    * AP-2 | [BE] Removed unnecessary codes.
    
    Co-authored-by: Md. Asaduzzaman Noor <asaduzzaman.noor@bongobd.com>

--- internal/app/model/entities.go
+++ internal/app/model/entities.go
@@ -1,4 +1,8 @@
 package model
 
 type User struct {
+	ID          int    `json:"id"`
+	PhoneNumber string `json:"phone_number" db:"phone_number"`
+	Password    string `json:"password"`
+	CategoryId  int    `json:"category_id" db:"category_id"`
 }
