fc4f367be3b41c328684c629c5cd5364e3d416ae
Ziaur Rahman <zrshishir@shadhinbd.com>
2021-06-23 11:30:55 +0600

    adding and testing server_test.go file

--- internal/app/server/server_test.go
+++ internal/app/server/server_test.go
@@ -1,21 +1,22 @@
 package server
 
 import (
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewServer(t *testing.T) {
-	bindServer := NewServer(":1000", nil)
+	bindServer := NewServer(":1000", nil, nil)
 	go bindServer.Run()
 	defer bindServer.Shutdown()
 	time.Sleep(1 * time.Second)
 
 	t.Run("test success run server", func(t *testing.T) {
-		s := NewServer(":2000", nil)
+		s := NewServer(":2000", nil, nil)
 		assert.NotNil(t, s)
 
 		go func() {
@@ -28,14 +29,14 @@ func TestNewServer(t *testing.T) {
 	})
 
 	t.Run("test failed run with gracefully shutdown", func(t *testing.T) {
-		s := NewServer(":1000", nil)
+		s := NewServer(":1000", nil, nil)
 		assert.NotNil(t, s)
 		assert.NoError(t, s.Run())
 		assert.NoError(t, s.Shutdown())
 	})
 
 	t.Run("test server ping handler", func(t *testing.T) {
-		handler := NewServer(":2000", nil).route()
+		handler := NewServer(":2000", nil, nil).route()
 		req, _ := http.NewRequest(http.MethodGet, "/ping", nil)
 		rr := httptest.NewRecorder()
 
