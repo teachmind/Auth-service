68299e102f9a633e204f1ddd82200a7dc301c1ec
Ziaur Rahman <zrshishir@shadhinbd.com>
2021-06-26 22:24:08 +0600

    removing alias full_name for db

--- internal/app/model/entities.go
+++ internal/app/model/entities.go
@@ -1,16 +1,9 @@
 package model
 
 type User struct {
-<<<<<<< HEAD
 	ID           int    `json:"id"`
 	Phone        string `json:"phone"`
-	FullName     string `json:"full_name" db:"full_name"`
+	FullName     string `json:"full_name"`
 	Password     string `json:"password"`
 	BusinessName string `json:"business_name" db:"business_name"`
-=======
-	ID          int    `json:"id"`
-	PhoneNumber string `json:"phone_number" db:"phone_number"`
-	Password    string `json:"password"`
-	CategoryId  int    `json:"category_id" db:"category_id"`
->>>>>>> main
 }
