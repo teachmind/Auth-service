809fafe41fb5ead71fe1e3ff3a6a1de0e2bf6ee3
Md. Asaduzzaman Noor <asaduzzaman.noor@bongobd.com>
2021-06-29 23:34:01 +0600

    AP-2 | [BE] Migration task has cascade missing issue fix.

--- migrations/0001_add_tables.up.sql
+++ migrations/0001_add_tables.up.sql
@@ -1,11 +1,15 @@
-CREATE TABLE IF NOT EXISTS users (
-  id serial PRIMARY KEY,
-  phone_number TEXT NOT NULL UNIQUE,
-  password TEXT,
-  category_id INT
-);
-
 CREATE TABLE IF NOT EXISTS user_category (
   id serial PRIMARY KEY,
   category_name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  id serial PRIMARY KEY,
+  category_id INT,
+  phone_number TEXT NOT NULL UNIQUE,
+  password TEXT,
+  CONSTRAINT category_id
+      FOREIGN KEY(category_id) 
+	      REFERENCES user_category(id)
+          ON DELETE CASCADE
 );
